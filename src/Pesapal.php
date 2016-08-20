490e9ae2ba4441918f24dd00003f5b26c88665de
Mohamed Omar <momar@aimun.org>
2016-08-20 20:24:17 +0300

    change pesapal file to use demo https and checkpaymentstatus fn

--- src/Pesapal.php
+++ src/Pesapal.php
@@ -90,133 +90,51 @@ class Pesapal
         return '<iframe src="'.$iframe_src.'" width="'.$params['width'].'" height="'.$params['height'].'" scrolling="auto" frameBorder="0"> <p>Unable to load the payment page</p> </iframe>';
     }
 
-    function redirectToIPN($pesapalNotification,$pesapal_merchant_reference,$pesapalTrackingId){
+    function checkPaymentStatus($pesapal_merchant_reference,$pesapalTrackingId) {
+      $consumer_key = config('pesapal.consumer_key');
 
-        $consumer_key = config('pesapal.consumer_key');
+      $consumer_secret = config('pesapal.consumer_secret');
 
-        $consumer_secret = config('pesapal.consumer_secret');
+      $statusrequestAPI = config('pesapal.live') ? "https://www.pesapal.com/api/querypaymentstatus" : "https://demo.pesapal.com/api/querypaymentstatus";
+       $token = $params = NULL;
+       $consumer = new OAuthConsumer($consumer_key, $consumer_secret);
+       $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
 
-        //$statusrequestAPI = config('pesapal.live') ? 'https://www.pesapal.com/api/querypaymentstatus' : 'http://demo.pesapal.com/api/querypaymentstatus';
-        
-        $statusrequestAPI = config('pesapal.live') ? 'https://www.pesapal.com/api/querypaymentdetails' : 'http://demo.pesapal.com/api/querypaymentdetails';
+       //get transaction status
+       $request_status = OAuthRequest::from_consumer_and_token($consumer, $token, "GET", $statusrequestAPI, $params);
+       $request_status->set_parameter("pesapal_merchant_reference", $pesapal_merchant_reference);
+       $request_status->set_parameter("pesapal_transaction_tracking_id",$pesapalTrackingId);
+       $request_status->sign_request($signature_method, $consumer, $token);
 
-        if($pesapalNotification=="CHANGE" && $pesapalTrackingId!='')
-        {
-           $token = $params = NULL;
-           $consumer = new OAuthConsumer($consumer_key, $consumer_secret);
-           $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
+       $ch = curl_init();
+       curl_setopt($ch, CURLOPT_URL, $request_status);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+       curl_setopt($ch, CURLOPT_HEADER, 1);
+       curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+       if(defined('CURL_PROXY_REQUIRED')) if (CURL_PROXY_REQUIRED == 'True')
+       {
+          $proxy_tunnel_flag = (defined('CURL_PROXY_TUNNEL_FLAG') && strtoupper(CURL_PROXY_TUNNEL_FLAG) == 'FALSE') ? false : true;
+          curl_setopt ($ch, CURLOPT_HTTPPROXYTUNNEL, $proxy_tunnel_flag);
+          curl_setopt ($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+          curl_setopt ($ch, CURLOPT_PROXY, CURL_PROXY_SERVER_DETAILS);
+       }
 
-           //get transaction status
-           $request_status = OAuthRequest::from_consumer_and_token($consumer, $token, "GET", $statusrequestAPI, $params);
-           $request_status->set_parameter("pesapal_merchant_reference", $pesapal_merchant_reference);
-           $request_status->set_parameter("pesapal_transaction_tracking_id",$pesapalTrackingId);
-           $request_status->sign_request($signature_method, $consumer, $token);
+       $response = curl_exec($ch);
+       $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+       $raw_header  = substr($response, 0, $header_size - 4);
+       $headerArray = explode("\r\n\r\n", $raw_header);
+       $header      = $headerArray[count($headerArray) - 1];
 
-           $ch = curl_init();
-           curl_setopt($ch, CURLOPT_URL, $request_status);
-           curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-           curl_setopt($ch, CURLOPT_HEADER, 1);
-           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-           if(defined('CURL_PROXY_REQUIRED')) if (CURL_PROXY_REQUIRED == 'True')
-           {
-              $proxy_tunnel_flag = (defined('CURL_PROXY_TUNNEL_FLAG') && strtoupper(CURL_PROXY_TUNNEL_FLAG) == 'FALSE') ? false : true;
-              curl_setopt ($ch, CURLOPT_HTTPPROXYTUNNEL, $proxy_tunnel_flag);
-              curl_setopt ($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
-              curl_setopt ($ch, CURLOPT_PROXY, CURL_PROXY_SERVER_DETAILS);
-           }
+       //transaction status
+       $elements = preg_split("/=/",substr($response, $header_size));
+       $status = $elements[1];
 
-           $response = curl_exec($ch);
-
-           $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-           $raw_header  = substr($response, 0, $header_size - 4);
-           $headerArray = explode("\r\n\r\n", $raw_header);
-           $header      = $headerArray[count($headerArray) - 1];
-
-           //transaction status
-           $elements = preg_split("/=/",substr($response, $header_size));
-           //$status = $elements[1];
-           $components = explode(',', $elements[1]);
-           $transaction_id = $components[0];
-           $payment_method = $components[1];
-           $merchant_reference = $components[3];
-           $status = $components[2];
-
-           curl_close ($ch);
-
-           if($status == 'PENDING'){
-              sleep(60);
-              redirectToIPN($pesapalNotification,$pesapal_merchant_reference,$pesapalTrackingId);
-            }
-           
-           //UPDATE YOUR DB TABLE WITH NEW STATUS FOR TRANSACTION WITH pesapal_transaction_tracking_id $pesapalTrackingId
-           $separator = explode('@', config('pesapal.ipn'));
-           $controller = $separator[0];
-           $method = $separator[1];
-           $class = '\App\Http\Controllers\\'.$separator[0];
-           $payment = new $class();
-           $payment -> $method($transaction_id,$status,$payment_method,$merchant_reference);
-
-           if($status != "PENDING")
-           {
-              $resp="pesapal_notification_type=$pesapalNotification&pesapal_transaction_tracking_id=$pesapalTrackingId&pesapal_merchant_reference=$pesapal_merchant_reference";
-              ob_start();
-              echo $resp;
-              ob_flush();
-              exit;
-           }
-        }
-
+       curl_close ($ch);
+      return $status;
+      
     }
 
-
-    function getMerchantStatus($pesapal_merchant_reference){
-
-         $consumer_key = config('pesapal.consumer_key');
-
-         $consumer_secret = config('pesapal.consumer_secret');
-
-         $statusrequestAPI = config('pesapal.live') ? 'https://www.pesapal.com/api/querypaymentstatusbymerchantref' : 'http://demo.pesapal.com/api/querypaymentstatusbymerchantref';
-          
-         $token = $params = NULL;
-         $consumer = new OAuthConsumer($consumer_key, $consumer_secret);
-         $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
-
-         //get transaction status
-         $request_status = OAuthRequest::from_consumer_and_token($consumer, $token, "GET", $statusrequestAPI, $params);
-         $request_status->set_parameter("pesapal_merchant_reference", $pesapal_merchant_reference);
-         $request_status->sign_request($signature_method, $consumer, $token);
-
-         $ch = curl_init();
-         curl_setopt($ch, CURLOPT_URL, $request_status);
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-         curl_setopt($ch, CURLOPT_HEADER, 1);
-         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-         if(defined('CURL_PROXY_REQUIRED')) if (CURL_PROXY_REQUIRED == 'True')
-         {
-            $proxy_tunnel_flag = (defined('CURL_PROXY_TUNNEL_FLAG') && strtoupper(CURL_PROXY_TUNNEL_FLAG) == 'FALSE') ? false : true;
-            curl_setopt ($ch, CURLOPT_HTTPPROXYTUNNEL, $proxy_tunnel_flag);
-            curl_setopt ($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
-            curl_setopt ($ch, CURLOPT_PROXY, CURL_PROXY_SERVER_DETAILS);
-         }
-
-         $response = curl_exec($ch);
-
-         $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-         $raw_header  = substr($response, 0, $header_size - 4);
-         $headerArray = explode("\r\n\r\n", $raw_header);
-         $header      = $headerArray[count($headerArray) - 1];
-
-         //transaction status
-         $elements = preg_split("/=/",substr($response, $header_size));
-         $status = $elements[1];
-
-         curl_close ($ch);
-
-         return $status;
-    }
-
-
-
+  
     public function random_reference($prefix = 'PESAPAL',$length = 15)
     {
         $keyspace = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
